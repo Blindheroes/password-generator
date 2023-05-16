db0f24dfe09abd2e84744510b781f8e306f85ac0
Blindheroes <95034324+Blindheroes@users.noreply.github.com>
2023-05-16 19:22:32 +0700

    stable version 1.0.1

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 let password = document.getElementById("password")
 let passwordLenght = document.getElementById("passLenght")
-
-
-
+let currentPass = document.getElementById("currentPass")
 
 function generatePass(len) {
-
-
+    if (len == '') len = 8
     const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
     const numberic = "1234567890"
@@ -23,6 +20,7 @@ function generatePass(len) {
 function getPass() {
     let newPass = generatePass(passwordLenght.value)
     password.value = newPass
+    currentPass.textContent = newPass
     console.log(passwordLenght.value)
     console.log(newPass)
 }
